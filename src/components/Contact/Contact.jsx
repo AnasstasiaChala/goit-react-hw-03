7ecb5f0cc0a083bbb1f916eb31b3f36f9066d78f
AnasstasiaChala <nastiachala12@gmail.com>
2024-07-01 18:12:06 +0300

    react3hmwrk done

---
+++ src/components/Contact/Contact.jsx
@@ -0,0 +1,13 @@
+import styles from './Contact.module.css'
+
+export default function Contact({ name, number, id, onDelete }) {
+    return (
+        <div className={styles.item}>
+            <div className={styles.data}>
+                <p className={styles.name}>{name}</p>
+                <p className={styles.number}>{number}</p>
+            </div>
+            <button className={styles.btn} onClick={() => onDelete(id)}>Delete</button>
+        </div>
+    );
+}
